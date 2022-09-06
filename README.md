fbe5e121a192fe681ac935dd17665310b8dd0805
DanCymonz <daniel.young@cymonz.com>
2022-09-07 00:53:45 +1200

    Add regions

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # countries
-a place to get countries data split out into names, and phone country codes
+Country information split out in different ways useful for binding
+to ui controls. The files are hosted in cloudflare with cors headers set so they can 
+be consumed via API
 
 
 - https://countries-api.pages.dev/all.json
@@ -7,5 +9,13 @@ a place to get countries data split out into names, and phone country codes
 - https://countries-api.pages.dev/names-alpha-3.json
 - https://countries-api.pages.dev/phone-alpha-2.json
 - https://countries-api.pages.dev/phone-alpha-3.json
+- https://countries-api.pages.dev/regions-alpha-2.json
+- https://countries-api.pages.dev/regions-alpha-3.json
 
-If you want the data refreshed, raise a PR bumping the package version
+If you want the data refreshed, raise a PR bumping the package version.
+Triggering a build will refresh the data
+
+Special thx to the follow resources
+- https://restcountries.com
+- https://github.com/dr5hn/countries-states-cities-database
+- cloudflare for hosting
--- src/main.js
+++ src/main.js
@@ -1,19 +1,47 @@
 import fetch from 'node-fetch';
-
-const x = await fetch('https://restcountries.com/v3.1/all')
 import fs from 'fs-extra'
+const restCountries = await fetch('https://restcountries.com/v3.1/all')
 
-const json = (await x.json()).sort((x, y) => {
+
+
+const restCountriesJson = (await restCountries.json()).sort((x, y) => {
     if (x.name.common < y.name.common) return -1;
     if (x.name.common > y.name.common) return 1;
     return 0;
 })
-await fs.writeJson('./dist/all.json', json, {spaces: 4})
-const names3 = json.map(x => ({name: x.name.common, cca3: x.cca3, flag: x.flag,}));
-const names2 = json.map(x => ({name: x.name.common, cca2: x.cca2, flag: x.flag,}));
+const regions = await fetch('https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/states.json').then(x =>x.json());
+
+
+
+await fs.writeJson('./dist/all.json', restCountriesJson, {spaces: 4})
+const names3 = restCountriesJson.map(x => ({name: x.name.common, cca3: x.cca3, flag: x.flag,}));
+const names2 = restCountriesJson.map(x => ({name: x.name.common, cca2: x.cca2, flag: x.flag,}));
+
+const names2To3Map = {};
+restCountriesJson.forEach(c => {
+    names2To3Map[c.cca2] = c.cca3;
+})
+
+const regionsByCountry2 = {};
+regions.forEach(region => {
+    if(!regionsByCountry2[region.country_code]) {
+        regionsByCountry2[region.country_code] = [];
+    }
+    regionsByCountry2[region.country_code].push(region.name);
+})
+
+const regionsByCountry3 = {};
+regions.forEach(region => {
+    const code = names2To3Map[region.country_code];
+    if(!code) throw new Error('no country code exists for ' + region.country_code)
+    if(!regionsByCountry3[code]) {
+        regionsByCountry3[code] = [];
+    }
+    regionsByCountry3[code].push(region.name);
+})
 
 const phoneAlpha3 = [];
-json.forEach(country => {
+restCountriesJson.forEach(country => {
     (country.idd?.suffixes || []).forEach(prefix => {
         phoneAlpha3.push({
             name: country.name.common,
@@ -25,7 +53,7 @@ json.forEach(country => {
 });
 
 const phoneAlpha2 = [];
-json.forEach(country => {
+restCountriesJson.forEach(country => {
     (country.idd?.suffixes || []).forEach(prefix => {
         phoneAlpha2.push({
             name: country.name.common,
@@ -40,3 +68,5 @@ await fs.writeJson('./dist/names-alpha-3.json', names3, {spaces: 4})
 await fs.writeJson('./dist/names-alpha-2.json', names2, {spaces: 4})
 await fs.writeJson('./dist/phone-alpha-3.json', phoneAlpha3, {spaces: 4})
 await fs.writeJson('./dist/phone-alpha-2.json', phoneAlpha2, {spaces: 4})
+await fs.writeJson('./dist/regions-alpha-2.json', regionsByCountry2, {spaces: 4})
+await fs.writeJson('./dist/regions-alpha-3.json', regionsByCountry3, {spaces: 4})
